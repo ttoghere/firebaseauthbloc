1b5419dc31cff8a51c0d74d5a4edf1b3e9d52a2b
ttoghere <59641232+ttoghere@users.noreply.github.com>
2023-02-28 11:02:38 +0300

    AuthDone
    Will add read.me and explanation comments

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebaseauthbloc/blocs/Auth/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   static const String routeName = "/home";
@@ -7,14 +9,26 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "HomePage",
-          style: Theme.of(context)
-              .textTheme
-              .headlineMedium!
-              .copyWith(color: Colors.white),
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              "HomePage",
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium!
+                  .copyWith(color: Colors.white),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                BlocProvider.of<AuthBloc>(context).add(SignOutRequestedEvent());
+              },
+              child: Text("SignOut"))
+        ],
       ),
     );
   }
